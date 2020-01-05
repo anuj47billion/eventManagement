c5d1050e97948d2941e2328a15b56ea49c359ce5
deepaktomar47 <anuj.gangrade@47billion.com>
2020-01-05 23:33:53 +0530

    Initial API

--- server/Routes/User.js
+++ server/Routes/User.js
@@ -3,4 +3,8 @@ const User = require('../Controllers/User');
 
 const router = express.Router();
 
+router.route('/login')
+  /* POST /v1.0/users/login - User login */
+  .post(User.login);
+
 module.exports = router;
