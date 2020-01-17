e3de2fca406f0173cedb11569de416199abdf9ad
deepaktomar47 <anuj.gangrade@47billion.com>
2020-01-17 22:46:52 +0530

    token work

--- env/development.js
+++ env/development.js
@@ -8,7 +8,7 @@ const devConfig = {
   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || '',
   DATABASE_PORT: process.env.DATABASE_PORT || 3306,
   DATABASE_DIALECT: process.env.DATABASE_DIALECT || 'mysql',
-  APP_SECRET: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
+  APP_SECRET: '3c0880de0c0ae9cebdad97d1e129c0005a19b1e99d24e3beb57b2fca296ca0bbd1bf288942322dd7fdeed595c8c5e60a',
   TOKEN_LIFE: 604800, // in seconds i.e 7 days
   // MAIL_ID: 'example@gmail.com',
   // MAIL_PASS: '123456789',
